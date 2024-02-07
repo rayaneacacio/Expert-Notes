cbc686f87c6fc130ad79a03621e69809f38f309d
rayaneacacio <rray74307@gmail.com>
2024-02-07 18:47:04 -0300

    update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import logo from "../../assets/logo.svg";
 import arrowUpRight from "../../assets/arrow-up-right.svg";
 
+import { NoteCard } from "../../components/NoteCard";
+
 import { Container } from "./style";
 
 export function Home() {
@@ -12,14 +14,19 @@ export function Home() {
 
       <div className="border"></div>
 
-      <div>
+      <main>
         <div className="newNote">
+          <button className="buttonIcon"> <img src={ arrowUpRight } alt="" /> </button>
           <h2>Adicionar nota</h2>
           <p>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
         </div>
 
+        <NoteCard date={ new Date() } content="O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo:" />
 
-      </div>
+        <NoteCard date={ new Date() } content="O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo:" />
+
+        <NoteCard date={ new Date() } content="O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo:" />
+      </main>
     </Container>
   )
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
+  padding: 5% 11%;
 
-  img {
+  > img {
     width: 12.4rem;
     height: 2.3rem;
   }
@@ -25,5 +25,22 @@ export const Container = styled.div`
     height: 25rem;
     padding: 2rem;
     border-radius: 6px;
+    position: relative;
+    /* flex: 1; */
+  }
+
+  main {
+    display: flex;
+    gap: 2.4rem;
+    flex-wrap: wrap;
+
+    > div, button {
+      flex-grow: 1;
+      /* flex: 1; */
+    }
+
+    > :last-child {
+      /* flex-grow: 0.03; */
+    }
   }
 `;
